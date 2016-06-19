5a999791bf708ed36e34b72e25b979792ee86542
Fin Tsai <fin.tsai86@gmail.com>
2016-06-19 15:02:29 +1000

    Get user current geolocation and save to database

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
+    lat_long = cookies[:lat_long]
+    if lat_long
+      @geolocation = lat_long.split("|")
+    end
+
+    if @geolocation && user_signed_in?
+      current_user.latitude = @geolocation[0].to_f
+      current_user.longitude = @geolocation[1].to_f
+      current_user.save
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160618062507) do
+ActiveRecord::Schema.define(version: 20160619041450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20160618062507) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
