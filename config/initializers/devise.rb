17c1e1f3d319e07363dc9a9d67993eb169d42f62
Fin Tsai <fin.tsai86@gmail.com>
2016-06-22 01:22:16 +1000

    Add DEVISE_KEY and SERVER_NAME env config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = 'bffedae244db3f4b7b94a76d83190a140a45713dc901d196381aab4cc0a7157e58a8ffadd483153be8b4f00ded58a48f4ae87d9653c13f491b4d0c3e1f71c74e'
+  config.secret_key = ENV["DEVISE_KEY"]
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
